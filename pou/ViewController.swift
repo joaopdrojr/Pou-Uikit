22d85b484f61aff8c49b04ed80a1be4e2cd2ada9
maria <maria>
2025-02-13 16:42:34 -0300

    ajustes no storyboard da main, inclusão dos assets e dos backgrounds

--- pou/ViewController.swift
+++ pou/ViewController.swift
@@ -1,14 +1,16 @@
-//
-//  ViewController.swift
-//  pou
-//
-//  Created by Joao Pedro Junior on 07/02/25.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Header: UIView!
+    
+    @IBOutlet weak var Comodo: UIStackView!
+    
+    @IBOutlet weak var Pou: UIImageView!
+    
+    @IBOutlet weak var Interactions: UIStackView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
