af6d35585ccfc1629e73c3c94f7b4f1ed0429595
Joao.pdro <joao.pdrojr@gmail.com>
2025-02-14 15:04:48 -0300

    Actions criados

--- pou/ViewController.swift
+++ pou/ViewController.swift
@@ -8,14 +8,43 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var Pou: UIImageView!
     
-    @IBOutlet weak var Interactions: UIStackView!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func geladeiraTapped(_ sender: Any) {
+        print("toquei na geladeira!")
+        
+    }
+    
+    @IBAction func leftTopButton(_ sender: Any) {
+        print("toquei botão topo esquerda")
+    }
+    
+    @IBAction func rightTopButton(_ sender: Any) {
+        print("toquei botão topo direita")
+    }
+    
+    @IBAction func leftBottomButton(_ sender: Any) {
+        print("toquei botão baixo esquerda")
+    }
+    
+    @IBAction func rightBottomButton(_ sender: Any) {
+        print("toquei botão baixo direita")
+    }
+    
+    @IBOutlet weak var pouTapped: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
