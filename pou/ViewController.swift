b6b02aaa865c031f18cf07ad81c7b0192edc9158
maria <maria>
2025-02-19 14:50:27 -0300

    trabalhando na main

--- pou/ViewController.swift
+++ pou/ViewController.swift
@@ -2,17 +2,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var Header: UIView!
+    @IBOutlet weak var background: UIImageView!
     
-    @IBOutlet weak var Comodo: UIStackView!
+    @IBOutlet weak var Header: UIView!
     
     @IBOutlet weak var Pou: UIImageView!
     
-    @IBOutlet weak var Interactions: UIStackView!
+    @IBOutlet weak var stool: UIImageView!
     
+    @IBOutlet weak var roomName: UILabel!
+    
+    @IBOutlet weak var foodChoice: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        background.image = UIImage(named: "backgroundKitchen")
+        Pou.image = UIImage(named: "pou")
+        stool.image = UIImage(named: "stool")
+        roomName.text = "Kitchen"
+        foodChoice.image = UIImage(named: "pizza")
+       
         // Do any additional setup after loading the view.
     }
 
