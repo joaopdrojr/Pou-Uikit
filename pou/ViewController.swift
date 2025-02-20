d9066870504bd0273778ba149498cfb4f6bb5abe
Joao.pdro <joao.pdrojr@gmail.com>
2025-02-20 17:31:39 -0300

    escolha de alimento funcional e drag n drop

--- pou/ViewController.swift
+++ pou/ViewController.swift
@@ -1,30 +1,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var background: UIImageView!
-    
-    @IBOutlet weak var Header: UIView!
-    
     @IBOutlet weak var Pou: UIImageView!
-    
     @IBOutlet weak var stool: UIImageView!
-    
     @IBOutlet weak var roomName: UILabel!
-    
     @IBOutlet weak var foodChoice: UIImageView!
+    
+    // Array de imagens para alternar
+    let foodImages = ["pizza", "icecream", "hotdog", "fries", "soda"]
+    var currentImageIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Configuração inicial das imagens
         background.image = UIImage(named: "backgroundKitchen")
         Pou.image = UIImage(named: "pou")
         stool.image = UIImage(named: "stool")
         roomName.text = "Kitchen"
-        foodChoice.image = UIImage(named: "pizza")
-       
-        // Do any additional setup after loading the view.
+        foodChoice.image = UIImage(named: foodImages[currentImageIndex])
+        
+        // Adicionar Gesture Recognizer para arrastar a foodChoice
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+        foodChoice.isUserInteractionEnabled = true
+        foodChoice.addGestureRecognizer(panGesture)
     }
-
-
+    
+    @objc func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: self.view)
+        
+        if let viewToDrag = gesture.view {
+            viewToDrag.center = CGPoint(x: viewToDrag.center.x + translation.x, y: viewToDrag.center.y + translation.y)
+            gesture.setTranslation(CGPoint.zero, in: self.view)
+        }
+    }
+    
+    @IBAction func rightFoodButton(_ sender: Any) {
+        currentImageIndex = (currentImageIndex + 1) % foodImages.count
+        foodChoice.image = UIImage(named: foodImages[currentImageIndex])
+    }
+    
+    @IBAction func leftFoodButton(_ sender: Any) {
+        currentImageIndex = (currentImageIndex - 1 + foodImages.count) % foodImages.count
+        foodChoice.image = UIImage(named: foodImages[currentImageIndex])
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
-
